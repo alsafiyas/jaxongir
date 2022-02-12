f0d6b50e311c1037107a625163228d99759c039b
alsafiyas <91256702+alsafiyas@users.noreply.github.com>
2022-02-12 08:17:17 +0500

    Add files via upload

---
+++ js.js
@@ -0,0 +1,36 @@
+// let a = Number(prompt("a="));
+// let b=Number(prompt("b="));
+// let c=a+3*(b-1);
+// console.log(` c =${c}`);
+// let a = Number(prompt("a ni kiriting"));
+// let z =Number(prompt("b ni kiriting"));
+// let b = a*3+4*z;
+// console.log(`b = ${b}`);
+
+
+// let a = Number(prompt("a="));
+// let b = Number(prompt("b="));
+// let c = Number(prompt("c="));
+// let d = a+b+c;
+// console.log(`d= ${d}`);
+
+
+// let a = Number(prompt("a="));
+// let S = a%3+4;
+// console.log(`S= ${S}` );
+
+
+// let a = Number(prompt("a="));
+// let b = Number(prompt("b="));
+// let c = Number(prompt("c="));
+// let s = a*b/c;
+// console.log(`s = ${s}`);
+
+
+// begin 1
+
+let a=Number(prompt=("son kiriting"))
+    b=Number(prompt=("2-soni kiriting"));
+let G=Math.sqrt(a*b);
+
+console.log`G = ${G}`;
